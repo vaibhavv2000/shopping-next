86b0bb0893ddfcbd06e79f79c981eab1e970821d
vaibhav <vaibhavk1965@gmail.com>
2024-07-18 20:00:03 +0530

    Updated & deployed on vercel

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+const URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "shopping-next-ivory.vercel.app";
 
 export const API = axios.create({
- baseURL: "http://localhost:3000/api",
+ baseURL: `${URL}/api`,
 });
