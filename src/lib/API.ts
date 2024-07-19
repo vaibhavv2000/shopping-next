c2b358af48fc2dec47412ca26d78bf17a3010b13
vaibhav <vaibhavk1965@gmail.com>
2024-07-19 15:02:32 +0530

    ...

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "shopping-next-ivory.vercel.app";
+let host = "shopping-next-ivory.vercel.app";
+
+const URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
 
 export const API = axios.create({
  baseURL: `${URL}/api`,
